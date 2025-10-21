075fe22eada93b8e03d785cf54010368097296c7
Nino-Techwoman <nini.bregvadze@icloud.com>
2025-10-21 10:06:35 +0400

    script

---
+++ script.js
@@ -0,0 +1,34 @@
+
+let FirstName = "Sarah";  
+let LastName = "Smith"      
+let age = 25;                 
+let email = "exsamole@me.com";
+let PhoneNumber = "+995555123456"; 
+let adult = true;        
+
+
+console.log( "Firstname:" , FirstName);
+console.log("Lastname:", LastName);
+console.log("Age:", age);
+console.log("Email:", email);
+console.log("Phone number:", PhoneNumber);
+
+
+console.log("%cPhone number: " + PhoneNumber, "color: red;");
+console.log("%cAdult:" + adult,"color: green");
+
+age = 43;
+
+console.log("changed Age is:" , age);
+
+
+
+
+
+
+
+
+
+
+
+
